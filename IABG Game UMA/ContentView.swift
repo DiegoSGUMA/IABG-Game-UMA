feb897fdd006c40aa0bfb25feb339b4aecd04d1d
Diego Sánchez <proyectodiegocjc@gmail.com>
2024-09-09 09:54:03 +0200

    Initial Commit

---
+++ IABG Game UMA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IABG Game UMA
+//
+//  Created by Diego Sánchez on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IABG Game UMA/IABG_Game_UMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  IABG_Game_UMAApp.swift
+//  IABG Game UMA
+//
+//  Created by Diego Sánchez on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct IABG_Game_UMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
